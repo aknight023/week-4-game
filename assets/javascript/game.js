160930040eee7406bc695dc594ba5822200016cb
anthonyknight023@gmail.com <anthonyknight023@gmail.com>
2018-01-06 18:57:23 -0500

    fix formating

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,7 @@
 $(document).ready(function() {
+       
+    var winCounter = loseCounter = userGuessTotal = gemNum1 = gemNum2 = gemNum3 = gemNum4 = 0;
     
-    var randomNum = Math.floor(Math.random() * ((120-19)+1) + 19);
-    var userGuessTotal = 0;
-    var winCounter = 0;
-    var loseCounter = 0;
-    var gemNum1 = Math.floor(Math.random() * ((12-1)+1) + 1);
-    var gemNum2 = Math.floor(Math.random() * ((12-1)+1) + 1);
-   	var gemNum3 = Math.floor(Math.random() * ((12-1)+1) + 1);
-   	var gemNum4 = Math.floor(Math.random() * ((12-1)+1) + 1);
-
 	var updateWin = function() {
 		$("#win").html("Wins: " + winCounter);
 	};
@@ -23,24 +16,26 @@ $(document).ready(function() {
 	    gemNum2 = Math.floor(Math.random() * ((12-1)+1) + 1);
 	   	gemNum3 = Math.floor(Math.random() * ((12-1)+1) + 1);
 	   	gemNum4 = Math.floor(Math.random() * ((12-1)+1) + 1);
+	   	updateWin();
+		updateLose();
 	   	$('.total').html(userGuessTotal); 
 	   	$(".randomNum").html(randomNum);
 	};
 	
 	var checkerStatus = function () {
 
-    $('.total').text(userGuessTotal); 
-	
-	if (userGuessTotal == randomNum){
-		winCounter ++;
-		updateWin();	
-		reset();
-	}
-	else if ( userGuessTotal > randomNum){
-		loseCounter++;
-		updateLose();	
-		reset();
-	}
+	    $('.total').text(userGuessTotal); 
+		
+		if (userGuessTotal == randomNum){
+			winCounter ++;
+			updateWin();	
+			reset();
+		}
+		else if ( userGuessTotal > randomNum){
+			loseCounter++;
+			updateLose();	
+			reset();
+		}
 	};
 
 	
@@ -48,29 +43,23 @@ $(document).ready(function() {
 	    userGuessTotal += gemNum1;    
 	    checkerStatus();
   	});
+
 	$('#two').on ('click', function(){
 	    userGuessTotal += gemNum2;    
 	    checkerStatus();
   	});
-
 
 	$('#three').on ('click', function(){
 	    userGuessTotal += gemNum3; 
 	    checkerStatus();
   	});
 
-
 	$('#four').on ('click', function(){
 	    userGuessTotal += gemNum4;    
 	    checkerStatus();
   	});
 
-
 	// initial load
-	$(".randomNum").html(randomNum);
-	updateWin();
-	updateLose();
-	$(".total").html(userGuessTotal);
-
+	reset();
 
 });
